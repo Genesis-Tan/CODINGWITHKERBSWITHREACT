b54733146ca8d593425ff20ca9ffb128580e803f
Genesis Tan <genesischarlestan3@gmail.com>
2023-05-29 16:57:22 +0800

    until console

--- js-fundamentals/09-classes/classes.js
+++ js-fundamentals/09-classes/classes.js
@@ -1,4 +1,43 @@
-(function () {
-  // write follow-along code below
-  
-})();
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  get area() {
+    return this.calcArea();
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+const square = new Rectangle(10, 10);
+console.log(square.area);
+console.log(square.calcArea());
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    console.log(`${this.name}: *noise*`);
+  }
+}
+
+const human = new Animal("Nina");
+human.speak(); // Nina: *noise*
+
+class Dog extends Animal {
+  // calls the super class constructor
+  // and passes in the name parameter
+  constructor(name) {
+    super(name);
+  }
+  speak() {
+    console.log(`${this.name}: woof`);
+  }
+}
+const dog = new Dog("Alexander");
+dog.speak(); // Alexander: woof
---
+++ js-fundamentals/JS-console/console.js
@@ -0,0 +1,25 @@
+// console.dir(window);
+// var person = { name: "John Doe", age: 25 };
+// console.dir(person);
+// console.dir(console);
+
+// console.time("response in");
+// alert("Click me");
+
+// console.timeEnd("response in");
+// alert("Click me again");
+
+// console.time("response in");
+// alert("CLICK MO KO!!!");
+
+// console.timeEnd("response in");
+
+console.log("%s has %d points", "John", 3);
+
+console.log(
+  "%cHello world!",
+  "color: blue; font-size: xx-large; font-family: Ponnala;"
+);
+
+console.assert(1 === 1);
+console.assert("one === 1", "one is not equal to 1");
